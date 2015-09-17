68d20b69d46020a6c086d72698b2ece5cb7bfeb2
Martin Halliday <martin.bikeman@gmail.com>
2015-09-17 00:02:17 -0700

    Added a cmd script to copy database persistence DLLs to server bin folder

---
+++ SetupPriusPersistence.cmd
@@ -0,0 +1,4 @@
+copy Urchin.Server.Persistence.Prius\bin\Debug\Prius*.dll Urchin.Server.Owin\bin\
+copy Urchin.Server.Persistence.Prius\bin\Debug\MySQL*.dll Urchin.Server.Owin\bin\
+copy Urchin.Server.Persistence.Prius\bin\Debug\Npgsql*.dll Urchin.Server.Owin\bin\
+copy Urchin.Server.Persistence.Prius\bin\Debug\Urchin.Server.Persistence.Prius.dll Urchin.Server.Owin\bin\
