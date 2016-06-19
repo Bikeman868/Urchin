2b7ac1dcbd964a95298219877d8a9ec203ac8cdf
Martin Halliday <martin.bikeman@gmail.com>
2016-06-19 16:48:57 -0700

    Ignore merge temp files on check in

--- Urchin.Client/Data/ConfigurationStore.cs
+++ Urchin.Client/Data/ConfigurationStore.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Urchin.Client.Interfaces;
