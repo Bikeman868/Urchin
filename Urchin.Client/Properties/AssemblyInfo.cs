070021539035f1b9925ccc456f4c78c604b4eca3
Martin Halliday <martin.bikeman@gmail.com>
2019-12-10 10:27:15 -0800

    Updated for Ioc modules version 1.4.1

--- Urchin.Client/Properties/AssemblyInfo.cs
+++ Urchin.Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Strongly typed configuration management system with change notification")]
 [assembly: AssemblyCompany("Martin Halliday")]
 [assembly: AssemblyProduct("Urchin")]
-[assembly: AssemblyCopyright("Copyright ©Martin Halliday 2015-2019")]
+[assembly: AssemblyCopyright("Copyright © Martin Halliday 2015-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.2.7")]
-[assembly: AssemblyFileVersion("1.2.7")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
